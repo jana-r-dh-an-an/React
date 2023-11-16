ec15619cf854139e07645eee76e57a8a2eef2a3d
janardhanan <rsjana305@gmail.com>
2023-11-16 16:39:04 +0530

    React_Episode_001

---
+++ BASIC_REACT_02/app.js
@@ -0,0 +1,3 @@
+const cr_head=React.createElement("h1",{},"Hello World From React!");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(cr_head);
---
+++ BASIC_REACT_03/app.js
@@ -0,0 +1,48 @@
+//myTask--001
+
+// const compo1=React.createElement('div',{id:"first_container"},[
+//    React.createElement('div',{id:"data1"},[
+//       React.createElement('h1',{id:"heading0"},"This is jana"),
+//        React.createElement('h2',{id:"heading1"},"this is also jana")
+//     ]),
+//     React.createElement('div',{id:"datas"},[
+//         React.createElement('h1',{id:"heading2"},"This is jana"),
+//          React.createElement('h2',{id:"heading3"},"this is also jana")
+//       ])
+
+// ]);
+// const root=ReactDOM.createRoot(document.getElementById("main"));
+// root.render(compo1);
+// console.log(compo1);
+
+
+//myTask--002
+
+const create_component=React.createElement('div',{class:"parent_class"},[
+    React.createElement('div',{class:"sub_parent"},[
+       React.createElement('p' ,{class:"Para_1"},"this is under sub_parent_para"),
+       React.createElement('h2',{class:"parent_2"},"this is under parent and heading")
+    ]),
+    React.createElement('div',{class:"sub_parent"},[
+        React.createElement('p' ,{class:"Para_1"},"this is under sub_parent_para"),
+        React.createElement('h2',{class:"parent_2"},"this is under parent and heading")
+     ])
+
+]);
+
+const create_component2=React.createElement('div',{class:"parent_class"},[
+    React.createElement('div',{class:"sub_parent"},[
+       React.createElement('p' ,{class:"Para_1"},"this is under sub_parent2_para"),
+       React.createElement('h2',{class:"parent_2"},"this is under parent2 and heading")
+    ]),
+    React.createElement('div',{class:"sub_parent"},[
+        React.createElement('p' ,{class:"Para_1"},"this is under sub_parent2_para"),
+        React.createElement('h2',{class:"parent_2"},"this is under parent2 and heading")
+     ])
+
+]);
+
+const root_rendering=ReactDOM.createRoot(document.getElementById("main"));
+const root_rendering2=ReactDOM.createRoot(document.getElementById("main"));
+root_rendering.render(create_component);
+root_rendering2.render(create_component2);
